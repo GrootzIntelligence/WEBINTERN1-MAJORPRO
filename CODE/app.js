6aee4316c0611110f292a1fe95d8e3ac6f46a90e
pulkit1417 <gupta.pulkit2408@gmail.com>
2024-09-29 23:07:22 +0530

    main commit

--- CODE/app.js
+++ CODE/app.js
@@ -1,18 +1,29 @@
-const express = require('express');
-const path = require('path'); 
+const express = require("express");
+const path = require("path");
 
 const app = express();
-PORT = 8000
+const PORT = 8000;
 
 app.set("view engine", "ejs");
-app.set("views", path.resolve("./views"))
+app.set("views", path.resolve("./views"));
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', async (req, res)=>{
-      res.render("home")
-    })
+app.get("/", async (req, res) => {
+  res.render("home");
+});
+
+app.get("/about", async (req, res) => {
+  res.render("about");
+});
+
+app.get('/main-blog', async(req, res) =>{
+  res.render('main-blog');
+})
+app.get('/single-blog', async(req, res) =>{
+  res.render('single-blog');
+})
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-})
+  console.log(`Server is running on port ${PORT}`);
+});
